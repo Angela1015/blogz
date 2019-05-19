f0add9b1a3f1245661b520ad4b6edf0c372df5bb
David Deberry <deberry1124@gmail.com>
2019-05-19 09:15:53 -0500

    added blog user relationship, modified title on login

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(240))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
        
-    def __init__(self, title,body):
+    def __init__(self, title,body,owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True)
     password = db.Column(db.String(20))
+    blogs = db.relationship('Blog', backref = 'owner')
    
     def __init__(self,username,password):
         self.username = username
@@ -46,9 +49,14 @@ def login():
         if user and user.password == password:
             session['username'] = username
             return redirect ('/newpost')
-        else:
+
+        elif  user and user.password != password:
             #TODO - explain why login failed
-            return '<h1>error!<h1>'
+            flash('Incorrect password')
+        else:
+            if not user:
+                flash('User does not exist')
+                return redirect ('/login')
         
     return render_template('login.html')
 @app.route('/register', methods =['POST','GET'])
